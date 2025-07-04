557c7cb096599673216195c99a81a50ef914d60a
Avi Stokar <avistokar@outlook.com>
2025-07-04 15:10:36 -0400

    Refactor HL7Message to use immutable collections

--- src/HL7Data Models/HL7Message.cs
+++ src/HL7Data Models/HL7Message.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace HL7;
 
 public class HL7Message {
-    public Dictionary<Type, List<IHL7Data>> HL7Records { get; } = new();
+    private readonly Dictionary<Type, List<IHL7Data>> dict = new();
 
-    public bool LoadHL7Message(string hl7Message) {
+    public IImmutableDictionary<Type, IImmutableList<IHL7Data>> HL7Records { get; private set; } = null!;
+    public MSH MSH => (MSH)HL7Records[typeof(MSH)].Single();
+    public ICollection<T> GetRecords<T>() where T : IHL7Data => HL7Records.TryGetValue(typeof(T), out var records) ? records.Cast<T>().ToArray() : [];
+
+    private HL7Message() { }
+
+    public static HL7Message? Create(string hl7Message) {
+        var msg = new HL7Message();
+        return msg.loadHL7Message(hl7Message) ? msg : null;
+    }
+
+    public static bool TryCreate(string hl7Message, out HL7Message? result) {
+        var msg = new HL7Message();
+        if (msg.loadHL7Message(hl7Message)) {
+            result = msg;
+            return true;
+        }
+        result = null;
+        return false;
+    }
+
+    private bool loadHL7Message(string hl7Message) {
         try {
             var message = Message.Parse(hl7Message);
             var msh = new MSH(message);
-            HL7Records.Add(typeof(MSH), [msh]);
+            dict.Add(typeof(MSH), [msh]);
             foreach (var segment in message.Segments.Where(s => s.Name != "MSH")) {
                 var hl7Data = HL7DataLoader.Create(segment);
                 var list = ensureTypeIsInDictionary(hl7Data);
                 list.Add(hl7Data);
             }
+
+            HL7Records = dict.ToImmutableDictionary(kvp => kvp.Key, IImmutableList<IHL7Data> (kvp) => kvp.Value.ToImmutableList());
             return true;
         } catch {
+            HL7Records = ImmutableDictionary<Type, IImmutableList<IHL7Data>>.Empty;
             return false;
         }
 
         List<IHL7Data> ensureTypeIsInDictionary(IHL7Data hl7Data) {
             var type = hl7Data.GetType();
-            return HL7Records.TryGetValue(type, out var list)
-                ? list 
-                : HL7Records[type] = [];
+            return dict.TryGetValue(type, out var list)
+                ? list
+                : dict[type] = [];
         }
     }
 }
