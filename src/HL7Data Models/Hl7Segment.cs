5f02cc032c8f114f42f26c77f4263cfdbec24d74
Avi Stokar <avistokar@outlook.com>
2025-07-18 10:46:06 -0400

    Refactor Hl7Segment serialization logic
    - Added necessary using directives for collections, IO, LINQ, and reflection.
    - Updated the `Serialize` method signature for improved readability.
    - Enhanced property retrieval and serialization handling, including support for generic collections.
    - Simplified initial string construction for serialization, particularly for the `MSH` type.
    - Introduced a local function `serializeProperty` for better property serialization structure.
    - Added checks for `Hl7DataType` to prevent null reference exceptions and suggested logging warnings.

--- src/HL7Data Models/Hl7Segment.cs
+++ src/HL7Data Models/Hl7Segment.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace HL7;
 
@@ -12,21 +14,23 @@ public abstract record Hl7Segment : Hl7ComplexType {
         }
     }
 
-    public override string Serialize(Hl7Encoding encoding, Hl7Structure? _) {
-        var props = this.GetProperties().Select(p => p.GetValue(this)).ToArray();
-        var initialString = "";
+    public override string Serialize(Hl7Encoding encoding, Hl7Structure? structure) {
+        var props = this.GetProperties().ToArray();
+        var type = this.GetType();
+        var initialString = $"{type.Name}{(type == typeof(MSH) ? encoding : "")}";
+        return initialString + string.Join(encoding.FieldDelimiter, props.Select(p => serializeProperty(p)));
 
-        if (this is MSH msh) {
-            initialString = $"{nameof(MSH)}{encoding}{encoding.FieldDelimiter}";
-            props = props[2..];
-        } else {
-            var type = this.GetType();
-            initialString = $"{type.Name}{encoding.FieldDelimiter}";
+        string serializeProperty(PropertyInfo propertyInfo) {
+            if (propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition() == typeof(ICollection<>)) {
+                var collection = propertyInfo.GetValue(this) as System.Collections.IEnumerable;
+                if (collection == null) return string.Empty;
+                var serializedItems = collection.OfType<Hl7DataType>().Select(hl7Item => hl7Item.Serialize(encoding, structure));
+                return string.Join(encoding.FieldDelimiter.ToString(), serializedItems);
+            }
+
+            var h = (propertyInfo.GetValue(this) as Hl7DataType);
+            if (h == null) return string.Empty;  //log a warning here 
+            return h.Serialize(encoding, Hl7Structure.Hl7Segment);
         }
-
-        return initialString + string.Join(
-            encoding.FieldDelimiter,
-            props.Select(v => (v as Hl7DataType)?.Serialize(encoding, Hl7Structure.Hl7Segment) ?? string.Empty)
-        );
     }
 }
