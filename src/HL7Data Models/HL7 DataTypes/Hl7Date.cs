ee24f7e5abd63336989057163aaac9f5f62d0b2e
Avi Stokar <avistokar@outlook.com>
2025-07-18 11:53:11 -0400

    Fix Hl7Date constructor to handle null values
    Updated the `Hl7Date` constructor to conditionally set the format only when the `val` parameter is not null. This change prevents potential issues related to null values and ensures proper formatting.

--- src/HL7Data Models/HL7 DataTypes/Hl7Date.cs
+++ src/HL7Data Models/HL7 DataTypes/Hl7Date.cs
@@ -8,7 +8,7 @@ public abstract record Hl7Date : Hl7SimpleType {
 
     protected Hl7Date(string? val, Hl7Structure structure = Hl7Structure.Hl7Component) : this(Hl7DateParser.ParseInstant(val), structure) {
         StringValue = val;
-        Format = Hl7DateParser.GetFormat(val);
+        if (val is not null) Format = Hl7DateParser.GetFormat(val);
     }
 
     protected Hl7Date(Instant? value, Hl7Structure structure = Hl7Structure.Hl7Component) : base(structure) {
