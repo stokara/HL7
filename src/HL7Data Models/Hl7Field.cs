c5c9c0f5fa0fdc82852524175d13768564ac22a0
Avi Stokar <avistokar@outlook.com>
2025-07-17 21:55:44 -0400

    Refactor Serialize method in Hl7Value<T> class
    The `Serialize` method has been restructured to improve clarity and efficiency. A new helper method, `tryGetSimpleResult`, has been introduced to handle simple serialization cases. If properties cannot be serialized simply, they are serialized individually and joined with a specified delimiter. This change enhances the handling of both simple and complex types in the serialization process.

--- src/HL7Data Models/Hl7Field.cs
+++ src/HL7Data Models/Hl7Field.cs
@@ -12,13 +12,26 @@ public abstract record Hl7Value<T> : Hl7ComplexType, IHl7Value where T : class, 
     protected Hl7Value(T value, Hl7Structure structure) : this(structure) => Value = value;
 
     public override string Serialize(Hl7Encoding encoding, Hl7Structure? sourceStructure = null) {
-        var props = this.GetProperties().Select(p => p.GetValue(this)).ToArray();
-        if (props.Select(v => (v as Hl7DataType)?.Complexity).Any(c => c == HL7.Complexity.Simple)) {
-            var nonEmpty = props.Select(v => (v as Hl7DataType)?.StringValue).Where(s=> !string.IsNullOrEmpty(s)).ToArray();
-            if (nonEmpty.Length == 0) return string.Empty;
-            if (nonEmpty.Length == 1) return nonEmpty.Single();
+        var props = this.GetProperties()
+            .Select(p => (Hl7DataType)p.GetValue(this)!)
+            .ToArray();
+        if (tryGetSimpleResult(props, out var simpleResult)) return simpleResult;
+       
+        var strings = props.Select(v => v.Serialize(encoding, Hl7Structure.Hl7Field));
+        return string.Join(encoding.ComponentDelimiter, strings );
+        
+        static bool tryGetSimpleResult(Hl7DataType[] props, out string simpleResult) {
+            var nonEmptyCount = 0;
+            simpleResult = string.Empty;
+            foreach (var item in props) {
+                if (item.Complexity == HL7.Complexity.Complex) return false;
+                if (!string.IsNullOrEmpty(item.StringValue)) {
+                    if (nonEmptyCount++ > 1) return false;
+                    simpleResult = item.StringValue!;
+                }
+            }
+            return true;
         }
-        return string.Join(encoding.ComponentDelimiter, props.Select(v => (v as Hl7DataType)?.Serialize(encoding, Hl7Structure.Hl7Field) ?? string.Empty));
     }
 }
 
